dcc2ed490d95e2ae17bb50346087325635234183
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-06-30 10:50:25 -0700

    full CRUD functionality for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,23 +5,20 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-
   end
 
   def new
     @product = Product.new
   end
 
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      render :new
-    end
-
   def edit
     @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   def update
@@ -33,12 +30,13 @@ class ProductsController < ApplicationController
     end
   end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to products_path
-  end
-
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
 end
 
 private
