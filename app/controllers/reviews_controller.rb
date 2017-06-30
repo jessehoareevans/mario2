00c188881bb073a69c3884f4d4eb2ba084994b68
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-06-30 12:49:26 -0700

    add full CRUD functionality to reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,49 @@
+class ReviewsController < ApplicationController
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(product_params)
+    if @review.save
+      redirect_to product_path(@review.product)
+    else
+      render :new
+    end
+end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @product = Task.find(params[:id])
+
+    if product_params[:description] === "done_products"
+      @product.update(done_products: true)
+
+      redirect_to product_path(@product.product)
+    elsif @product.update(product_params)
+      redirect_to product_path(@product.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+      @product = Task.find(params[:id])
+      @product.destroy
+      redirect_to product_path(@product.product)
+    end
+
+
+private
+  def product_params
+    params.require(:review).permit(:author, :content, :rating)
+  end
+end
