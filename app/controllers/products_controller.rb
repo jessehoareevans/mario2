b01ceedce8124a829ad0868233cd4051addfa874
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-06-30 10:43:25 -0700

    make inital specs pass

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+
   end
 
   def new
@@ -18,6 +19,26 @@ class ProductsController < ApplicationController
     else
       render :new
     end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 end
 
 private
